b67a26672b335a2ea8aaff9659c8c3b140ca65c8
Andrew Tapper <andrew@tapper.codes>
2022-08-04 18:59:54 -0500

    context and schema, local graphiQL is up

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,6 +1,6 @@
 import { createServer } from '@graphql-yoga/node'
-import { schema } from '@lib/schema'
-import { context } from '@lib/context'
+import { schema } from '@/lib/schema'
+import { context } from '@/lib/context'
 
 const server = createServer({
   schema,
